0e3989d391ef3c43310c2b44cc799c344411d556
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-09 00:02:23 +0300

    Modify controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,20 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: %i[show destroy]
+
   def show
-    @course = Course.find(params[:id])
     render json: @course
   end
 
-  def delete
-    @course = Course.find(params[:id])
+  def destroy
     @course.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Course not found' }, status: :not_found
   end
 end
