6e1b932439f364f6fdb9fb458d6c787b68abe396
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-06-08 12:20:33 -0500

    Fix linters and update version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_many :reservations
-  validates :name, presence: true, length: { minimum: 3 } , uniqueness: true
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
 end
