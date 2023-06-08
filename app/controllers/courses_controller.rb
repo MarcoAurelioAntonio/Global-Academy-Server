c207a4ad6edfba62311ed50955748d17f514fbb0
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-08 23:44:01 +0300

    Update controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,11 @@
 class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
+    render json: @course
   end
 
   def delete
     @course = Course.find(params[:id])
     @course.destroy
-    redirect_to courses_path
   end
 end
