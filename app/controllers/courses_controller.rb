063f0f79219a80443d8da065f833444308f7c2e1
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-06-08 15:55:19 -0500

    Create index and create actions for courses_controller & modify Course model type field to course_type

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,17 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+    render json: { courses: @courses }
+  end
+
+  def create
+    @new_course = Course.new(params.require(:course).permit(:name, :start_date, :end_date, :description,
+                                                            :course_type, :price))
+    @new_course.save!
+    if @new_course.save
+      render json: @new_course, status: :created
+    else
+      render json: @new_course.errors, status: :unprocessable_entity
+    end
+  end
 end
