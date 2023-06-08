16d3b33f257b55e2fa38c96ee709f03cc727e18b
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-06-08 17:01:19 -0300

    Fix offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-	def index
-		@users = User.all.order(:name).limit(10)
-		render json: JSON.pretty_generate(@users.as_json)
-		#vrender json: @users, only: [:name]
-	end
+  def index
+    @users = User.all.order(:name).limit(10)
+    render json: JSON.pretty_generate(@users.as_json)
+    # vrender json: @users, only: [:name]
+  end
 end
