5c06484124449ebb86be5faaa9dd8f05d913e9c8
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-08 23:41:29 +0300

    Add show and delete controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,11 @@
 class CoursesController < ApplicationController
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def delete
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path
+  end
 end
