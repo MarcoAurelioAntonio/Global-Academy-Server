fa7ce04e6096bfb39b555c71892ee8d42ddf554b
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-06-08 19:10:38 -0300

    fis offenses

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
-	def index
-		@user = User.includes(:reservations).find(params[:user_id])
+  def index
+    @user = User.includes(:reservations).find(params[:user_id])
     @reservations = @user.reservations
     render json: JSON.pretty_generate(@reservations.as_json(only: [:created_at]))
   end
