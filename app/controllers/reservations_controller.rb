85f95776ece248404d4321b5f6a6d33bfb4b35a8
Kender <kenderromain@gmail.com>
2023-06-08 20:44:03 -0400

    Set up the endpoint for adding reservation.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,19 @@ class ReservationsController < ApplicationController
     @reservations = @user.reservations
     render json: JSON.pretty_generate(@reservations.as_json(only: [:created_at]))
   end
+
+  # POST /reservations
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reservation_params
+    params.require(:reservation).permit(:user_id,:course_id)
+  end
 end
