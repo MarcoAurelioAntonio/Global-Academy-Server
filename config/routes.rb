b79dde156ee2604a5b225eb3fdaa87034a4963d6
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-06-09 10:13:59 -0500

    Modify routes.rb delete duplicated resources of courses and add .env into gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :courses, only: [:index, :create]
   resources :users, only: [:index,:create] do
     resources :reservations, only: [:index, :create]
   end
